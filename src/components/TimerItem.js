992aca680b6a52ccbe7c26b408510b2792a3dacc
xander <shuaibualexander@gmail.com>
2021-09-27 20:38:31 +0100

    split exchanges

--- src/components/TimerItem.js
+++ src/components/TimerItem.js
@@ -90,6 +90,7 @@ function TimerItem() {
           bg="transparent"
           width="40px"
           radius="12px"
+
         >
           <Image src="./listings/huobi.png" srcSet="./listings/huobi@2x.png 2x, ./listings/huobi@3x.png 3x" />
         </Anchor>
