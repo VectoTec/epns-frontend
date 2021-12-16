f4521dac19bace7bed12ed1c7cc7d9488c697623
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-12-16 11:50:34 +0400

    Updates

---
+++ precache-manifest.8968f1a361eb4a8bf91612f77d1a7ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd74c3c7628767c320d9d269e73cf8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec97033ec4fdb8eb7ec9",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "57a7117489df9b27d624",
+    "url": "/static/css/main.e48dc770.chunk.css"
+  },
+  {
+    "revision": "ec97033ec4fdb8eb7ec9",
+    "url": "/static/js/2.d52c9f8d.chunk.js"
+  },
+  {
+    "revision": "bc4afa37962f9b856391545ea94c3a64",
+    "url": "/static/js/2.d52c9f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a7117489df9b27d624",
+    "url": "/static/js/main.deae83b4.chunk.js"
+  },
+  {
+    "revision": "3e234450efee118b7e12",
+    "url": "/static/js/runtime-main.fb220953.js"
+  }
+]);
